6cb971381c58641220869b039dbbc2573bc4a2ce
Kuddleman <edl1155@gmail.com>
2021-02-08 08:32:13 -0800

    write getters

--- src/Accounts.java
+++ src/Accounts.java
@@ -4,5 +4,38 @@ public class Accounts {
     private int accountNumber;
     private int initialDeposit;
 
+    // constructor for single owner account
+    public Accounts(String firstName, int anAccountNumber, int anInitialDeposit) {
+        this.name1 = firstName;
+        this.accountNumber = anAccountNumber;
+        this.initialDeposit = anInitialDeposit;
+    }
+
+    // constructor for joint-owner account
+    public Accounts(String firstName, String secondName, int anAccountNumber, int anInitialDeposit) {
+        this.name1 = firstName;
+        this.name2 = secondName;
+        this.accountNumber = anAccountNumber;
+        this.initialDeposit = anInitialDeposit;
+    }
+
+    // Getters
+    public String getName1() {
+        return name1;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getInitialDeposit() {
+        return initialDeposit;
+    }
+
+
 
 }
